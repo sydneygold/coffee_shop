9eac1bdd94bae8adbabc5758e6f8352aa17420e5
sbrook13 <sbrook13@gmail.com>
2020-08-18 17:53:08 -0600

    added multiple order options, need random picker method still

--- lib/models/cli.rb
+++ lib/models/cli.rb
@@ -8,7 +8,7 @@ class Cli
     def start
         system "clear"
         puts "WELCOME TO THE DIRTY BEAN COFFEE SHOP!" 
-        sleep(1.25)
+        sleep(1)
         puts "Can I get your name?"
         @name = gets.strip
         system "clear"
@@ -16,7 +16,7 @@ class Cli
         sleep(0.5)
         puts "\n"
         puts "I'd be happy to help get the perfectly curated cup for you."
-        sleep(1.25)
+        sleep(1.5)
         puts "\n"
         coffee_or_tea
     end    
@@ -27,25 +27,37 @@ class Cli
         
         choices = %w(Tea Coffee)
         type = prompt.select("What type of drink would you like?", choices, symbols: { marker: ">" })
-                if type == "Tea"
-                    puts "Sorry this is just a coffeehouse. But we highly recommend the Teahouse in Boulder!"
-                else
-                    system "clear"
-
-                    menu_options = ["I don't know, surprise me!", "I need help deciding..", "I'll come back another time."]
-                    choice = prompt.select("You came to the right place! Do you know what you want to order?", menu_options)
-                        if choice == "I have an idea of what I want..."
-                            puts "Great, let's get some more info."
-                            drink_options
-                        elsif choice == "I don't know, surprise me!"
-                            random_drink
-                        else
-                            puts "Goodbye #{@name}!"
-                            sleep(1.5)
-                            abort
-                        end        
-                end  
+            if type == "Tea"
+                puts "Sorry this is just a coffeehouse. But we highly recommend the Teahouse in Boulder!"
+                sleep(1)
+                start_over = prompt.yes?("Do you to order a coffee instead?")
+                    if start_over == true
+                        system "clear"
+                        order_options
+                    else
+                        goodbye
+                    end
+            else
+                system "clear"
+                order_options                 
+            end  
     end  
+
+    def order_options
+        prompt = TTY::Prompt.new
+        menu_options = ["I need help deciding..", "I don't know, surprise me!", "I'll come back another time. >> EXIT"]
+        choice = prompt.select("You came to the right place! Do you know what you want to order?", menu_options)
+            if choice == "I need help deciding.."
+                system "clear"
+                puts "Great, let's get some more info."
+                puts "\n"
+                drink_options
+            elsif choice == "I don't know, surprise me!"
+                random_drink
+            else
+                goodbye
+            end       
+    end
 
     def drink_options
         prompt = TTY::Prompt.new(symbols: {marker: '>'})
@@ -60,7 +72,9 @@ class Cli
             if sweet == true
                 syrup = prompt.select("What flavor do you want in your drink?", %w(Vanilla Mocha Caramel Coconut))
             else
-                puts "You're sweet enough!"
+                puts "\n ~~ Yeah... you're sweet enough! ~~"
+                puts "\n"
+                sleep(0.5)
             end
         temp = prompt.select("Do you want it hot or cold?", %w(Hot Iced Blended)) 
         system "clear"
@@ -91,4 +105,12 @@ class Cli
         
         # drinks = Drink.where(milk: milk, sweet: sweet)                    
     end
+
+    def goodbye
+        system "clear"
+        puts "Goodbye, " + @name.capitalize + "!"
+        sleep(1.5)
+        abort
+    end    
+
 end
