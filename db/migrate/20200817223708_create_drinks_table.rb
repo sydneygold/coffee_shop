59260461b3692cbfbc1cd3694b9a872e657e2064
sbrook13 <sbrook13@gmail.com>
2020-08-18 10:35:08 -0600

    created tables

--- db/migrate/20200817223708_create_drinks_table.rb
+++ db/migrate/20200817223708_create_drinks_table.rb
@@ -1,4 +1,15 @@
 class CreateDrinksTable < ActiveRecord::Migration[6.0]
   def change
+    create_table :drinks do |t|
+      t.string :name
+      t.text :description
+      t.boolean :milk
+      t.boolean :sweet
+      t.boolean :caffeine
+      t.string :hot_iced
+      t.string :milk_type
+      t.string :syrup
+    end
   end
 end
+
--- db/migrate/20200817223753_create_orders_table.rb
+++ db/migrate/20200817223753_create_orders_table.rb
@@ -1,4 +1,9 @@
 class CreateOrdersTable < ActiveRecord::Migration[6.0]
   def change
+    create_table :orders do |t|
+      t.string :date
+      t.references :user
+      t.references :drink
+    end
   end
 end
